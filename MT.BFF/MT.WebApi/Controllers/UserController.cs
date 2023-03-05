019cfc410abd97cb5fded70bf60c7fdee6103e10
TamasG <tamas.gczi@gmail.com>
2023-03-05 21:12:32 +0100

    Created backend project, added signin endpoint

---
+++ MT.BFF/MT.WebApi/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MT.WebApi.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MT.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<SignInToken>> GetJWTBearerToken(string email, string password)
+        {
+            var client = new HttpClient();
+
+            var json = JsonConvert.SerializeObject(new User(email, password));
+
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("http://217.13.111.9:5010/Authentication/SignIn", data);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ MT.BFF/MT.WebApi/Models/SignInToken.cs
@@ -0,0 +1,16 @@
+﻿namespace MT.WebApi.Models
+{
+    public class SignInToken
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+
+        public SignInToken(string userId, string token, DateTime expiresAt)
+        {
+            UserId = userId;
+            Token = token;
+            ExpiresAt = expiresAt;
+        }
+    }
+}
